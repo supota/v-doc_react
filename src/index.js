6ea3f6dadeac203ec5cf61c2b028af678b0c6bbb
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-08-14 18:39:27 +0900

    Update yui.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import './yui.css'
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
 
