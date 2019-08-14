e8002b31061839f916b5c2872c2b0f56022daad4
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-08-14 17:37:17 +0900

    Add header/footer

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+function Header() {
+  return (
+        <header>
+            <p>Header</p>
+        </header>
+  );
+}
+
+export default Header;
